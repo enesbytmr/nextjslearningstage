5aa0ba38bc9b1fbd400264efe7c7be943bfbbd74
enesbytmr <enesbytmr@gmail.com>
2024-01-28 13:06:16 +0300

    css implemente etme, ternary oerators, using lists , how to use map, reduce, filter etc , in react

--- src/App.js
+++ src/App.js
@@ -2,21 +2,33 @@
 import './App.css';
 
 function App() {
-
+  const age = 14;
+  const isBlue = true
+  const names = ["Pedro" , "Enes" , "Eray"];
   return (
-  <User name="Enes" age={24} email="enes@enes.com" />
+     <div className='App'>{age>= 18 ? <h1>OverAge</h1> : <h1> Under Age</h1>}
+     <h1 style={{ color: isBlue ? "blue": "red"}}>This is A colored TEXT</h1>
+
+     {isBlue && <button className='App'> This is a button</button>}
+     
+     <h2 className='App'>{names[0]}</h2>
+     <h2 className='App'>{names[1]}</h2>
+     <h2 className='App'>{names[2]}</h2>
+     
+     {/* cok daha kisa ve kullanisli */}
+
+
+     {names.map((name, key)=>{
+      return <h1 key={key}> {name }</h1>
+
+     })}
+     
+     </div>
+
+     
+
   );
 }
 
-const User = (props) => {
-return (
-  <div>
-  <h1>{props.name}</h1>
-  <h2>{props.age}</h2>
-  <h3>{props.email}</h3>
-  </div>
-)
-
-}
 
 export default App;
