0842f38f7b7061e8bdef3fae65ecc5bf157325d9
enesbytmr <enesbytmr@gmail.com>
2024-01-28 03:53:39 +0300

    props kullanimlarini ogrendim . component mantigi ve kullanim sekilleri ogrenildi.

--- src/App.js
+++ src/App.js
@@ -2,11 +2,21 @@
 import './App.css';
 
 function App() {
+
   return (
-    <div className="App">
-      <h1>Hello World!</h1>
-    </div>
+  <User name="Enes" age={24} email="enes@enes.com" />
   );
+}
+
+const User = (props) => {
+return (
+  <div>
+  <h1>{props.name}</h1>
+  <h2>{props.age}</h2>
+  <h3>{props.email}</h3>
+  </div>
+)
+
 }
 
 export default App;
